c757cd04a26e63fc8cae253e606e11fb43289ebd
li121314 <984815634@qq.com>
2018-01-18 23:26:07 +0800

    20180118

---
+++ itxiuzhengyuan/07/js/07.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+/************点击盒子设置小盒子的显示雨隐藏 hide：隐藏 ；show：显示**********************/
+
+$(".art-box-two").hide();
+/*$(".art-box").click(function () {
+    $(this).find(".art-box-two").show();
+});*/
+$(".art-box").click(function () {
+        $(".art-box-two").hide();  //清除上一个点击的样式
+        $(this).find(".art-box-two").show();
+
+    });
+
+
+/* ********************************************
+* *********************************************
+* **播放声音************************************
+* paused: 暂停判断****************************
+* play()播放********************************
+* pause（）暂停***************************
+* ************************************
+* *********************************************/
+$("#play").click(function () {
+
+
+    if($("#player")[0].paused){
+        $("#player")[0].play();
+
+    }
+    else {
+        $("#player")[0].pause();
+    }
+
+});
+
+$(".home").click(function () {
+        location.href='index.html';
+    });
+    $(".foot-one").click(function () {
+        location.href='index.html';
+    });
+
+});
