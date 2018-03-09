d4ed8de5f10a0b20efa3c77b88890cf7db80136f
li121314 <984815634@qq.com>
2018-03-09 21:50:21 +0800

    1

--- itxiuzhengyuan/10/js/renwu10.js
+++ itxiuzhengyuan/10/js/renwu10.js
@@ -1,4 +1,4 @@
-$(function () {
+/*$(function () {
    $(".top-right-a1").mouseenter(function () {
         $(".top-right-a1").css({"background":"white","color":"#000","width":"10rem"});
        $(".top-right-a2").css({"background":"none","color":"white","width":"5rem","border-right":"1px solid white"});
@@ -11,4 +11,4 @@ $(function () {
 
 
 
-});
+});*/
