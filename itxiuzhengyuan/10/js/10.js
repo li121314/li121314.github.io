aa5c5bc6a06022cd28032ffbbc39492b3bce3e86
li121314 <984815634@qq.com>
2018-09-15 20:42:17 +0800

    11

---
+++ itxiuzhengyuan/10/js/10.js
@@ -0,0 +1,10 @@
+$(function () {
+
+
+
+    $(".head-a1").hover(function () {
+        $(".head-a1").css({"background":"white","color":"#000"});
+        $(this).css({"background":"#1d7ad9","color":"white"});
+
+    });
+});
